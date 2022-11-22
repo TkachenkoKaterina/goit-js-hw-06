25b15f1c544e5039f747044c85fe887a481e1f50
Katerina <90516132+TkachenkoKaterina@users.noreply.github.com>
2022-11-22 23:41:19 +0200

    Try Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,35 +1,42 @@
 "use strict";
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+const inputRef = document.querySelector("input");
+const btnCreateRef = document.querySelector("button[data-create]");
+const btnDestroyRef = document.querySelector("button[data-destroy]");
+const divBoxesRef = document.querySelector("#boxes");
 
-// const inputRef = document.querySelector("input");
-// console.log(inputRef);
+inputRef.addEventListener("input", onInput);
+divBoxesRef.addEventListener("click", createBoxes);
 
-// const btnCreateRef = document.querySelector("button[data-create]");
-// console.log(btnCreateRef);
+function createBoxes(amount) {
+  let NextSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    divBoxesRef.append(div);
+    div.style.width = "30px";
+    div.style.height = "30px";
+    getRandomHexColor(div);
+    // div.style.width = `"${NextSize}px"`;
+    console.log(`"${NextSize}px"`);
+    // div.style.height = `"${NextSize}px"`;
+    NextSize += 10;
+    // console.log(NextSize);
+    // console.log(div);
+  }
+}
 
-// const btnDestroyRef = document.querySelector("button[data-destroy]");
-// console.log(btnDestroyRef);
+function onInput(event) {
+  const amount = +event.currentTarget.value;
+  //   console.log(amount);
+  if (amount > 1 && amount < 100) {
+    createBoxes(amount);
+    console.log(`Все ок, в пределах допуска: ${amount}`);
+    return amount;
+  } else {
+    console.log(`За пределами допуска: ${amount}`);
+  }
+}
 
-// const divBoxesRef = document.querySelector("#boxes");
-// console.log(divBoxesRef);
-
-// function onInput(event) {
-//   const inputValue = +event.currentTarget.value;
-//   console.log(typeof inputValue);
-//   if (1 < inputValue < 100) {
-//     console.log(`Все ок, в пределах допуска: ${inputValue}`);
-//   } else if (1 > inputValue > 100) {
-//     console.log(`За пределами допуска: ${inputValue}`);
-//   }
-// }
-
-// inputRef.addEventListener("input", onInput);
-
-// console.log(5);
-// console.log("1");
-
-// const a = "1";
-// console.log(typeof +a);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
