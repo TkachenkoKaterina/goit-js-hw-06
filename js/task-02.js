4fe9804ddd5039e8206e4badd3a1a333868f9587
Katerina <90516132+TkachenkoKaterina@users.noreply.github.com>
2022-11-22 22:20:21 +0200

    Переделка 2-го задания

--- js/task-02.js
+++ js/task-02.js
@@ -9,36 +9,49 @@ const ingredients = [
   "Condiments",
 ];
 
-const list0 = document.createElement("li");
-list0.textContent = ingredients[0];
-const listClass = list0.classList.add("item");
-console.log(list0);
-
-const list1 = document.createElement("li");
-list1.textContent = ingredients[1];
-const listClass1 = list1.classList.add("item");
-console.log(list1);
-
-const list2 = document.createElement("li");
-list2.textContent = ingredients[2];
-const listClass2 = list2.classList.add("item");
-console.log(list2);
-
-const list3 = document.createElement("li");
-list3.textContent = ingredients[3];
-const listClass3 = list3.classList.add("item");
-console.log(list3);
-
-const list4 = document.createElement("li");
-list4.textContent = ingredients[4];
-const listClass4 = list4.classList.add("item");
-console.log(list4);
-
-const list5 = document.createElement("li");
-list5.textContent = ingredients[5];
-const listClass5 = list5.classList.add("item");
-console.log(list5);
+const ingredientsToRef = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  console.log(li);
+  return li;
+});
 
 const ulByIdRef = document.querySelector("#ingredients");
-const listItems = ulByIdRef.append(list0, list1, list2, list3, list4, list5);
+const listItems = ulByIdRef.append(...ingredientsToRef);
 console.log(ulByIdRef);
+
+// someRef.append(...itemsNodes);
+
+// const list0 = document.createElement("li");
+// list0.textContent = ingredients[0];
+// const listClass = list0.classList.add(".item");
+// console.log(list0);
+
+// const list1 = document.createElement("li");
+// list1.textContent = ingredients[1];
+// const listClass1 = list1.classList.add(".item");
+// console.log(list1);
+
+// const list2 = document.createElement("li");
+// list2.textContent = ingredients[2];
+// const listClass2 = list2.classList.add(".item");
+// console.log(list2);
+
+// const list3 = document.createElement("li");
+// list3.textContent = ingredients[3];
+// const listClass3 = list3.classList.add(".item");
+// console.log(list3);
+
+// const list4 = document.createElement("li");
+// list4.textContent = ingredients[4];
+// const listClass4 = list4.classList.add(".item");
+// console.log(list4);
+
+// const list5 = document.createElement("li");
+// list5.textContent = ingredients[5];
+// const listClass5 = list5.classList.add(".item");
+// console.log(list5);
+
+// const ulByIdRef = document.querySelector("#ingredients");
+// const listItems = ulByIdRef.append(list0, list1, list2, list3, list4, list5);
+// console.log(ulByIdRef);
